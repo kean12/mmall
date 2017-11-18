8e10e86c8a19a8872baca9bfeb735921fd05ed7f
Keane <kean12@126.com>
2017-11-18 15:59:19 +0800

    6-3课程结束

--- src/main/java/com/mmall/pojo/Category.java
+++ src/main/java/com/mmall/pojo/Category.java
@@ -31,6 +31,8 @@ public class Category {
         super();
     }
 
+
+
     public Integer getId() {
         return id;
     }
@@ -61,6 +63,21 @@ public class Category {
 
     public void setStatus(Boolean status) {
         this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return id != null ? id.equals(category.id) : category.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 
     public Integer getSortOrder() {
